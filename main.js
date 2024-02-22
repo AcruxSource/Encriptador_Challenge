5603412967f67404a1ea7870651ae214f708ec81
acruxsource <barandicajuanc@gmail.com>
2024-02-22 12:23:04 -0500

    agrega funcion  IIFE multi-Eventos, objeto text vacio y una function elementCatch para construir el object

--- main.js
+++ main.js
@@ -2,33 +2,41 @@ log = console.log;
 const inputText = document.getElementById("text__box");
 const lineNumber = document.querySelector(".box__input--lin");
 const columnNumber = document.querySelector(".box__input--col");
+const text = {} || undefined;
 
-inputText.addEventListener("keyup", () => {
-  lineNumber.textContent = positionStatus().lin;
-  columnNumber.textContent = positionStatus().col;
-});
-inputText.addEventListener("click", (ev) => {
-  lineNumber.textContent = positionStatus().lin;
-  columnNumber.textContent = positionStatus().col;
-});
-inputText.addEventListener("select", () => {
-  lineNumber.textContent = " -";
-  columnNumber.textContent = "-";
-});
+(function x(...typeEvent) {
+  typeEvent.forEach((element) => {
+    inputText.addEventListener(element, (ev) => {
+      elementCatch(this);
+      lineNumber.textContent = this.txt.length;
+      // lineNumber.textContent = positionStatus().lin;
+      // columnNumber.textContent = positionStatus().col;
 
-function positionStatus() {
-  const text = inputText.value;
+      if (ev.type === "input") {
+        // positionStatus();
+      }
+      if (ev.type === "select") {
+        lineNumber.textContent = " -";
+        columnNumber.textContent = "-";
+      }
+    });
+  });
+}).apply(text, ["click", "keyup", "select", "input"]);
+
+function elementCatch(text) {
+  const text1 = inputText.value;
+  text.txt = inputText.value;
   const position = inputText.selectionStart;
   var charsCount = 0;
-  const textArray = text.substring(0, position).split("\n");
-  for (var i = 0; i < textArray.length - 1; i++)
-    charsCount += textArray[i].length + 1;
-  return {
-    lin: textArray.length,
-    col: position - charsCount + 1,
-  };
-}
 
+  const textArray = text1.substring(0, position).split("\n");
+  log(textArray);
+  for (let i = 0; i < textArray.length - 1; i++) {
+    var textLength = textArray[i].length;
+    charsCount += textLength + 1;
+  }
+  log(position, charsCount);
+}
 function clean() {
   inputText.value = "";
 }
